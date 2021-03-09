7e05ee8cacec99671c368f613e93a520a4ad5017
Kaktushose <faulie50@gmail.com>
2021-03-09 15:07:28 +0100

    add numbers 0 to 9 to EmoteType

--- src/main/java/de/kaktushose/discord/reactionwaiter/EmoteType.java
+++ src/main/java/de/kaktushose/discord/reactionwaiter/EmoteType.java
@@ -4,13 +4,12 @@ package de.kaktushose.discord.reactionwaiter;
  * Some emotes that might be frequently used.
  *
  * @author Kaktushose
- * @version 1.0.0
+ * @version 2.0.0
  * @since 1.0.0
  *
  */
 
 public enum EmoteType {
-
 
     /**
      * Thumbs Up <br>
@@ -43,6 +42,12 @@ public enum EmoteType {
     CROSSMARK("\u274C"),
 
     /**
+     * Digit 0 <br>
+     * code point: U+0030
+     */
+    ZERO("\u0030"),
+
+    /**
      * Digit 1 <br>
      * code point: U+0031
      */
@@ -61,6 +66,42 @@ public enum EmoteType {
     THREE("\u0033"),
 
     /**
+     * Digit 4 <br>
+     * code point: U+0034
+     */
+    FOUR("\u0034"),
+
+    /**
+     * Digit 5 <br>
+     * code point: U+0035
+     */
+    FIVE("\u0035"),
+
+    /**
+     * Digit 6 <br>
+     * code point: U+0036
+     */
+    SIX("\u0036"),
+
+    /**
+     * Digit 7 <br>
+     * code point: U+0037
+     */
+    SEVEN("\u0037"),
+
+    /**
+     * Digit 8 <br>
+     * code point: U+0038
+     */
+    EIGHT("\u0038"),
+
+    /**
+     * Digit 9 <br>
+     * code point: U+0039
+     */
+    NINE("\u0039"),
+
+    /**
      * Triangular flag on post <br>
      * code point: U+1F6A9
      */
@@ -76,20 +117,34 @@ public enum EmoteType {
     }
 
     /**
-     * Returns the suitable emote for the given number. Only supports Integers in range from 0 to 2, inclusive.
+     * Returns the suitable emote for the given number.
      *
-     * @param i the Integer that will be represented as an emote
+     * @param i the Integer that will be represented as an emote, supports numbers von 0 to 9
      * @return the EmoteType representing the number
      */
 
     public static EmoteType getNumber(int i) {
         switch (i) {
             case 0:
-                return ONE;
+                return ZERO;
             case 1:
-                return TWO;
+                return ONE;
             case 2:
+                return TWO;
+            case 3:
                 return THREE;
+            case 4:
+                return FOUR;
+            case 5:
+                return FIVE;
+            case 6:
+                return SIX;
+            case 7:
+                return SEVEN;
+            case 8:
+                return EIGHT;
+            case 9:
+                return NINE;
             default:
                 return null;
         }
