e269b3f50bc8c7cad519ca48e8685f76a3e65d09
Kaktushose <faulie50@gmail.com>
2021-03-09 15:52:16 +0100

    refactor ReactionEvent

--- src/main/java/de/kaktushose/discord/reactionwaiter/ReactionEvent.java
+++ src/main/java/de/kaktushose/discord/reactionwaiter/ReactionEvent.java
@@ -1,17 +1,22 @@
 package de.kaktushose.discord.reactionwaiter;
 
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.MessageBuilder;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.events.message.guild.react.GuildMessageReactionAddEvent;
 import net.dv8tion.jda.api.requests.RestAction;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.function.Consumer;
 
 /**
  * Represents a reaction event.
  * This class is similar to {@code GuildMessageReactionAddEvent}, but it provides some extra utilities to work with.
  *
  * @author Kaktushose
- * @version 1.0.0
+ * @version 2.0.0
  * @since 1.0.0
  */
 
@@ -37,65 +42,139 @@ public class ReactionEvent extends GuildMessageReactionAddEvent {
     }
 
     /**
-     * Sends a response in the channel where the reaction event was triggered.
-     * Returns the RestAction to e.g. access the queue consumer.
+     * Sends a response to the channel where the reaction event was triggered.
      *
-     * @param message the String that will be send
-     * @return the RestAction
+     * @param message the message to send
      */
-    public RestAction<Message> respond(String message) {
-        return channel.sendMessage(message);
+    public void reply(@Nonnull String message) {
+        getChannel().sendMessage(message).queue();
     }
 
     /**
-     * Sends a response as an embed in the channel where the reaction event was triggered.
-     * Returns the RestAction to e.g. access the queue consumer.
+     * Sends a formatted message using the specified format string and arguments to the channel where the reaction event was triggered.
      *
-     * @param embedBuilder the EmbedBuilder that will be send
-     * @return the RestAction
+     * @param format the message to send
+     * @param args   Arguments referenced by the format specifiers in the format string. If there are more arguments than
+     *               format specifiers, the extra arguments are ignored. The number of arguments is variable and may be
+     *               zero.
+     * @throws java.util.IllegalFormatException If a format string contains an illegal syntax, a format specifier that
+     *                                          is incompatible with the given arguments, insufficient arguments given
+     *                                          the format string, or other illegal conditions.
      */
-    public RestAction<Message> respond(EmbedBuilder embedBuilder) {
-        return channel.sendMessage(embedBuilder.build());
+    public void reply(@Nonnull String format, @Nullable Object... args) {
+        reply(String.format(format, args));
     }
 
     /**
-     * Sends a response in the channel where the reaction event was triggered.
-     * Returns the RestAction to e.g. access the queue consumer.
-     * This method might be useful in combination with the MessageBuilder.
+     * Sends a response to the channel where the reaction event was triggered. This method also allows to access the JDA RestAction
+     * consumer.
      *
-     * @param message the message that will be send
-     * @return the RestAction
+     * @param message the message to send
+     * @param success the JDA RestAction success consumer
+     * @see <a href="https://ci.dv8tion.net/job/JDA/javadoc/net/dv8tion/jda/api/requests/RestAction.html">JDA RestAction Documentation</a>
      */
-    public RestAction<Message> respond(Message message) {
-        return channel.sendMessage(message);
+    public void reply(@Nonnull String message, @Nullable Consumer<Message> success) {
+        getChannel().sendMessage(message).queue(success);
+    }
+
+    /**
+     * Sends a response to the channel where the reaction event was triggered.
+     *
+     * @param message the {@code Message} to send
+     */
+    public void reply(@Nonnull Message message) {
+        getChannel().sendMessage(message).queue();
+    }
+
+    /**
+     * Sends a response to the channel where the reaction event was triggered. This method also allows to access the JDA RestAction
+     * consumer.
+     *
+     * @param message the {@code Message} to send
+     * @param success the JDA RestAction success consumer
+     * @see <a href="https://ci.dv8tion.net/job/JDA/javadoc/net/dv8tion/jda/api/requests/RestAction.html">JDA RestAction Documentation</a>
+     */
+    public void reply(@Nonnull Message message, @Nullable Consumer<Message> success) {
+        getChannel().sendMessage(message).queue(success);
+    }
+
+    /**
+     * Sends a response to the channel where the reaction event was triggered.
+     *
+     * @param messageBuilder the {@code MessageBuilder} to send
+     */
+    public void reply(@Nonnull MessageBuilder messageBuilder) {
+        getChannel().sendMessage(messageBuilder.build()).queue();
+    }
+
+    /**
+     * Sends a response to the channel where the reaction event was triggered. This method also allows to access the JDA RestAction
+     * consumer.
+     *
+     * @param messageBuilder the {@code MessageBuilder} to send
+     * @param success        the JDA RestAction success consumer
+     * @see <a href="https://ci.dv8tion.net/job/JDA/javadoc/net/dv8tion/jda/api/requests/RestAction.html">JDA RestAction Documentation</a>
+     */
+    public void reply(@Nonnull MessageBuilder messageBuilder, @Nullable Consumer<Message> success) {
+        getChannel().sendMessage(messageBuilder.build()).queue(success);
+    }
+
+    /**
+     * Sends a response to the channel where the reaction event was triggered.
+     *
+     * @param embedBuilder the {@code EmbedBuilder} to send
+     */
+    public void reply(@Nonnull EmbedBuilder embedBuilder) {
+        getChannel().sendMessage(embedBuilder.build()).queue();
+    }
+
+    /**
+     * Sends a message to the TextChannel where the command was called. This method also allows to access the JDA RestAction
+     * consumer.
+     *
+     * @param embedBuilder the {@code EmbedBuilder} to send
+     * @param success      the JDA RestAction success consumer
+     * @see <a href="https://ci.dv8tion.net/job/JDA/javadoc/net/dv8tion/jda/api/requests/RestAction.html">JDA RestAction Documentation</a>
+     */
+    public void reply(@Nonnull EmbedBuilder embedBuilder, @Nullable Consumer<Message> success) {
+        getChannel().sendMessage(embedBuilder.build()).queue(success);
     }
 
     /**
      * Sends a direct message to the user who triggered the reaction event.
      *
-     * @param message the String that will be send
+     * @param message the message to send
      */
-    public void sendPrivateMessage(String message) {
+    public void sendPrivateMessage(@Nonnull String message) {
+        executor.getUser().openPrivateChannel().queue(privateChannel -> privateChannel.sendMessage(message).queue());
+    }
+
+    /**
+     * Sends a direct message to the user who triggered the reaction event.
+     * This method might be useful in combination with the MessageBuilder.
+     *
+     * @param message the {@code Message} to send send
+     */
+    public void sendPrivateMessage(@Nonnull Message message) {
         executor.getUser().openPrivateChannel().queue(privateChannel -> privateChannel.sendMessage(message).queue());
     }
 
     /**
      * Sends an Embed via direct message to the user who triggered the reaction event.
      *
-     * @param embedBuilder the EmbedBuilder that will be send
+     * @param messageBuilder the {@code MessageBuilder} that will be send
      */
-    public void sendPrivateMessage(EmbedBuilder embedBuilder) {
-        executor.getUser().openPrivateChannel().queue(privateChannel -> privateChannel.sendMessage(embedBuilder.build()).queue());
+    public void sendPrivateMessage(@Nonnull MessageBuilder messageBuilder) {
+        executor.getUser().openPrivateChannel().queue(privateChannel -> privateChannel.sendMessage(messageBuilder.build()).queue());
     }
 
     /**
-     * Sends a direct message to the user who triggered the reaction event.
-     * This method might be useful in combination with the MessageBuilder.
+     * Sends an Embed via direct message to the user who triggered the reaction event.
      *
-     * @param message the message that will be send
+     * @param embedBuilder the {@code EmbedBuilder} to send
      */
-    public void sendPrivateMessage(Message message) {
-        executor.getUser().openPrivateChannel().queue(privateChannel -> privateChannel.sendMessage(message).queue());
+    public void sendPrivateMessage(@Nonnull EmbedBuilder embedBuilder) {
+        executor.getUser().openPrivateChannel().queue(privateChannel -> privateChannel.sendMessage(embedBuilder.build()).queue());
     }
 
 }
