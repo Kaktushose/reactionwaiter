07c8eb0ce76a28e1000c45d7eecc72281e27d2ab
Kaktushose <faulie50@gmail.com>
2021-03-09 15:50:42 +0100

    refactor ReactionWaiter

--- src/main/java/de/kaktushose/discord/reactionwaiter/ReactionWaiter.java
+++ src/main/java/de/kaktushose/discord/reactionwaiter/ReactionWaiter.java
@@ -1,7 +1,15 @@
 package de.kaktushose.discord.reactionwaiter;
 
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.Message;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import java.util.Arrays;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
 
 /**
  * The ReactionWaiter waits for reactions being added to messages.
@@ -9,91 +17,140 @@ import java.util.List;
  * Further you can also only accept specific members.
  *
  * @author Kaktushose
- * @version 1.0.0
+ * @version 2.0.0
  * @since 1.0.0
  */
 
 public class ReactionWaiter {
 
-    private List<String> emotes;
-    private long messageId;
-    private long memberId;
-    private Runnable runnable;
+    private final Set<String> emotes;
+    private final Message message;
+    private final Member member;
+    private Consumer<ReactionEvent> consumer;
 
     /**
      * This constructor creates a reaction waiter that will accept the given emotes, all messages and all users
      *
      * @param emotes the emotes that will be listened for
      */
-    public ReactionWaiter(String... emotes) {
-        this.emotes = Arrays.asList(emotes);
-        this.messageId = 0;
-        this.memberId = 0;
+    public ReactionWaiter(@Nonnull String... emotes) {
+        this.emotes = new HashSet<>(Arrays.asList(emotes));
+        this.message = null;
+        this.member = null;
     }
 
     /**
      * This constructor creates a reaction waiter that will accept the given emotes and all users but is limited to a specific message.
      *
-     * @param messageId the id of the message that will be monitored
-     * @param emotes the emotes that will be listened for
+     * @param message the message that will be monitored
+     * @param emotes  the emotes that will be listened for
      */
-    public ReactionWaiter(long messageId, String... emotes) {
-        this.emotes = Arrays.asList(emotes);
-        this.messageId = messageId;
-        this.memberId = 0;
+    public ReactionWaiter(@Nullable Message message, @Nonnull String... emotes) {
+        this.emotes = new HashSet<>(Arrays.asList(emotes));
+        this.message = message;
+        this.member = null;
     }
-
 
     /**
      * This constructor creates a reaction waiter that will accept the given emotes and is limited to a specific message and user.
      *
-     * @param userId the user that will be accepted
-     * @param messageId the id of the message that will be monitored
-     * @param emotes the emotes that will be listened for
+     * @param member  the member that will be accepted
+     * @param message the message that will be monitored
+     * @param emotes  the emotes that will be listened for
      */
-    public ReactionWaiter(long userId, long messageId, String... emotes) {
-        this.emotes = Arrays.asList(emotes);
-        this.messageId = messageId;
-        this.memberId = userId;
+    public ReactionWaiter(@Nullable Message message, @Nullable Member member, @Nonnull String... emotes) {
+        this.emotes = new HashSet<>(Arrays.asList(emotes));
+        this.message = message;
+        this.member = member;
     }
 
     /**
      * This method gets invoked when a GuildMessageReactionAddEvent matches the specifications given by the used constructor.
+     * This method will also activate the waiter.
      *
-     * @param runnable the {@link Runnable} containing the {@link ReactionEvent}
+     * @param consumer the callback that will be called when a {@link ReactionEvent} got triggered
+     * @return the current instance to use fluent interface
      */
-    public void onEvent(Runnable runnable) {
-        this.runnable = runnable;
+    public ReactionWaiter onEvent(@Nonnull Consumer<ReactionEvent> consumer) {
+        ReactionListener.addReactionWaiter(this);
+        this.consumer = consumer;
+        return this;
     }
 
     /**
-     * Activates the reaction waiter.
+     * This method gets invoked when a GuildMessageReactionAddEvent matches the specifications given by the used constructor.
+     * Additionally deactivates the reaction waiter after a given set of time. This method will also activate the waiter.
+     *
+     * @param consumer        the callback that will be called when a {@link ReactionEvent} got triggered
+     * @param removeReactions {@code true} if this method should also remove all reactions from the message
+     * @param delay           the time from now to deactivate the waiter
+     * @param timeUnit        the time unit of the delay parameter
+     * @return the current instance to use fluent interface
      */
-    public void startWaiting() {
+    public ReactionWaiter onEvent(@Nonnull Consumer<ReactionEvent> consumer, boolean removeReactions, long delay, @Nonnull TimeUnit timeUnit) {
         ReactionListener.addReactionWaiter(this);
+        ReactionListener.removeReactionWaiter(this, removeReactions, delay, timeUnit);
+        this.consumer = consumer;
+        return this;
     }
 
     /**
      * Deactivates the reaction waiter.
+     *
+     * @param removeReactions {@code true} if this method should also remove all reactions from the message
+     * @return the current instance to use fluent interface
      */
-    public void stopWaiting() {
-        ReactionListener.removeReactionWaiter(this);
+    public ReactionWaiter stopWaiting(boolean removeReactions) {
+        ReactionListener.removeReactionWaiter(this, removeReactions);
+        return this;
     }
 
-    List<String> getEmotes() {
+    /**
+     * Deactivates the reaction waiter after a given set of time.
+     *
+     * @param removeReactions {@code true} if this method should also remove all reactions from the message
+     * @param delay           the time from now to deactivate the waiter
+     * @param timeUnit        the time unit of the delay parameter
+     * @return the current instance to use fluent interface
+     */
+    public ReactionWaiter stopWaitingAfter(boolean removeReactions, long delay, @Nonnull TimeUnit timeUnit) {
+        ReactionListener.removeReactionWaiter(this, removeReactions, delay, timeUnit);
+        return this;
+    }
+
+    /**
+     * Get a Set containing the emotes that this waiter listens for.
+     *
+     * @return a Set containing the emotes that will be listened to
+     */
+    public Set<String> getEmotes() {
         return emotes;
     }
 
-    long getMessageId() {
-        return messageId;
+    /**
+     * Get the {@code Message} that this waiter listens for. Might be null.
+     *
+     * @return the {@code Message} that will be listened to
+     */
+    public Message getMessage() {
+        return message;
     }
 
-    void called(ReactionEvent event) {
-        runnable.run(event);
+    /**
+     * Get the {@code Member} that this waiter listens for. Might be null.
+     *
+     * @return the {@code Member} that will be listened to
+     */
+    public Member getMember() {
+        return member;
     }
 
-    long getMemberId() {
-        return memberId;
+    /**
+     * Get the {@link Consumer} that will be called when a {@link ReactionEvent} got triggered.
+     *
+     * @return the callback that will be called when a {@link ReactionEvent} got triggered
+     */
+    public Consumer<ReactionEvent> getConsumer() {
+        return consumer;
     }
-
 }
